aae5de9fc5233c572e520e05b393f3f95ec1a944
Tiernan Messmer <tiernan.messmer@gmail.com>
2025-08-10 00:26:55 +1000

    Initial commit

---
+++ go.mod
@@ -0,0 +1,24 @@
+module github.com/nvx/subspace-relay-pcsc-consumer-demo
+
+go 1.24.0
+
+require (
+	github.com/nvx/go-apdu v1.0.0
+	github.com/nvx/go-subspace-relay v0.0.1
+	github.com/nvx/subspace-relay v0.0.1
+)
+
+require (
+	github.com/Code-Hex/dd v1.1.0 // indirect
+	github.com/ansel1/merry/v2 v2.2.2 // indirect
+	github.com/eclipse/paho.golang v0.22.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/thessem/zap-prettyconsole v0.5.2 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	go.uber.org/zap/exp v0.3.0 // indirect
+	golang.org/x/net v0.43.0 // indirect
+	google.golang.org/protobuf v1.36.7 // indirect
+)
---
+++ subspace-relay-pcsc-consumer-demo.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"github.com/nvx/go-apdu"
+	subspacerelay "github.com/nvx/go-subspace-relay"
+	subspacerelaypb "github.com/nvx/subspace-relay"
+	"log/slog"
+	"os"
+	"strings"
+)
+
+// This can be set at build time using the following go build command:
+// go build -ldflags="-X 'main.defaultBrokerURL=mqtts://user:pass@example.com:1234'"
+var defaultBrokerURL string
+
+func main() {
+	ctx := context.Background()
+	var (
+		relayID    = flag.String("relay-id", "", "Subspace Relay ID to connect to")
+		brokerFlag = flag.String("broker-url", "", "MQTT Broker URL")
+	)
+	flag.Parse()
+
+	subspacerelay.InitLogger("subspace-relay-pcsc-consumer-demo")
+
+	brokerURL := subspacerelay.NotZero(*brokerFlag, os.Getenv("BROKER_URL"), defaultBrokerURL)
+	if brokerURL == "" {
+		slog.ErrorContext(ctx, "No broker URI specified, either specify as a flag or set the BROKER_URI environment variable")
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	if *relayID == "" {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Subspace Relay Client ID (PCSC HF): ")
+		var err error
+		*relayID, err = reader.ReadString('\n')
+		if err != nil {
+			return
+		}
+		*relayID = strings.TrimSpace(*relayID)
+	}
+
+	// The connection type can be omitted to allow connecting to any PCSC reader (direct or regular), or changed to
+	// ConnectionType_CONNECTION_TYPE_PCSC_DIRECT to instead ensure the remote end is a direct connection
+	reader, err := subspacerelay.NewPCSC(ctx, brokerURL, *relayID, subspacerelaypb.ConnectionType_CONNECTION_TYPE_PCSC)
+	if err != nil {
+		slog.ErrorContext(ctx, "Error opening Subspace Relay PCSC connection", subspacerelay.ErrorAttrs(err))
+		os.Exit(1)
+	}
+
+	err = run(ctx, reader)
+	if err != nil {
+		slog.ErrorContext(ctx, "Error", subspacerelay.ErrorAttrs(err))
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, reader *subspacerelay.PCSC) (err error) {
+	defer subspacerelay.DeferWrap(&err)
+
+	capduBytes, err := apdu.Capdu{
+		CLA: 0xFF,
+		INS: 0xCA, // GET DATA
+		P1:  0x00, // UID
+		P2:  0x00,
+		Ne:  apdu.MaxLenResponseDataStandard,
+	}.Bytes()
+	if err != nil {
+		return
+	}
+
+	rapduBytes, err := reader.Exchange(ctx, capduBytes)
+	if err != nil {
+		return
+	}
+
+	rapdu, err := apdu.ParseRapdu(rapduBytes)
+	if err != nil {
+		return
+	}
+
+	slog.InfoContext(ctx, "Got UID", "rapdu", rapdu)
+
+	return nil
+}
