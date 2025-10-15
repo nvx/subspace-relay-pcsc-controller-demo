7c9f009a318c01f9be423d3f8e26ef13c088421f
Tiernan Messmer <tiernan.messmer@gmail.com>
2025-10-15 22:43:29 +1000

    Add discovery and tweak naming

--- go.mod
+++ go.mod
@@ -1,28 +1,30 @@
-module github.com/nvx/subspace-relay-pcsc-consumer-demo
+module github.com/nvx/subspace-relay-pcsc-controller-demo
 
 go 1.25.0
 
 require (
 	github.com/nvx/go-apdu v1.0.1
 	github.com/nvx/go-rfid v0.0.2
-	github.com/nvx/go-subspace-relay v0.0.7
-	github.com/nvx/go-subspace-relay-logger v0.0.1
-	github.com/nvx/subspace-relay v0.0.9
+	github.com/nvx/go-subspace-relay v0.0.8
+	github.com/nvx/go-subspace-relay-logger v0.0.2
+	github.com/nvx/subspace-relay v1.0.4
 )
 
 require (
 	github.com/Code-Hex/dd v1.1.0 // indirect
-	github.com/ansel1/merry/v2 v2.2.2 // indirect
+	github.com/ansel1/merry/v2 v2.2.3 // indirect
 	github.com/bobg/go-generics/v4 v4.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/eclipse/paho.golang v0.23.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/thessem/zap-prettyconsole v0.5.2 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	go.uber.org/zap/exp v0.3.0 // indirect
-	golang.org/x/net v0.44.0 // indirect
+	golang.org/x/net v0.46.0 // indirect
 	golang.org/x/sync v0.17.0 // indirect
-	google.golang.org/protobuf v1.36.9 // indirect
+	google.golang.org/protobuf v1.36.10 // indirect
 )
---
+++ subspace-relay-pcsc-controller-demo.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"crypto/ecdh"
+	"crypto/rand"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"github.com/nvx/go-apdu"
+	"github.com/nvx/go-rfid"
+	subspacerelay "github.com/nvx/go-subspace-relay"
+	"github.com/nvx/go-subspace-relay-logger"
+	subspacerelaypb "github.com/nvx/subspace-relay"
+	"log/slog"
+	"os"
+	"strings"
+)
+
+// This can be set at build time using the following go build command:
+// go build -ldflags="-X 'main.defaultBrokerURL=mqtts://user:pass@example.com:1234'"
+var defaultBrokerURL string
+
+func main() {
+	ctx := context.Background()
+	var (
+		err          error
+		relayID      = flag.String("relay-id", "", "Subspace Relay ID to connect to")
+		useDiscovery = flag.Bool("discovery", false, "Use discovery")
+		discoveryKey = flag.String("discovery.secure", "*", "When discovery is enabled use this private key, if * a random key will be generated and printed. If empty will use plaintext discovery")
+		brokerFlag   = flag.String("broker-url", "", "MQTT Broker URL")
+		capdu        = flag.String("capdu", "FFCA000000", "cAPDU to run")
+	)
+	flag.Parse()
+
+	srlog.InitLogger("subspace-relay-pcsc-controller-demo")
+
+	brokerURL := subspacerelay.NotZero(*brokerFlag, os.Getenv("BROKER_URL"), defaultBrokerURL)
+	if brokerURL == "" {
+		slog.ErrorContext(ctx, "No broker URI specified, either specify as a flag or set the BROKER_URI environment variable")
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	capduBytes, err := hex.DecodeString(*capdu)
+	if err != nil {
+		slog.ErrorContext(ctx, "Error decoding cAPDU hex bytes", rfid.ErrorAttrs(err))
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	if *useDiscovery {
+		if *relayID != "" {
+			slog.ErrorContext(ctx, "Cannot specify relay id with discovery")
+			flag.Usage()
+			os.Exit(2)
+		}
+
+		var key *ecdh.PrivateKey
+		if *discoveryKey == "*" {
+			key, err = ecdh.X25519().GenerateKey(rand.Reader)
+			if err != nil {
+				slog.ErrorContext(ctx, "Error generating key", rfid.ErrorAttrs(err))
+				os.Exit(1)
+			}
+			slog.InfoContext(ctx, "Generated key", rfid.LogHex("public_key", key.PublicKey().Bytes()), rfid.LogHex("private_key", key.Bytes()))
+		} else if *discoveryKey != "" {
+			var keyBytes []byte
+			keyBytes, err = hex.DecodeString(*discoveryKey)
+			if err != nil {
+				slog.ErrorContext(ctx, "Error decoding discovery key hex bytes", rfid.ErrorAttrs(err))
+				flag.Usage()
+				os.Exit(2)
+			}
+
+			key, err = ecdh.X25519().NewPrivateKey(keyBytes)
+			if err != nil {
+				slog.ErrorContext(ctx, "Error parsing provided discovery private key", rfid.ErrorAttrs(err))
+				flag.Usage()
+				os.Exit(2)
+			}
+
+			slog.InfoContext(ctx, "Using discovery key", rfid.LogHex("public_key", key.PublicKey().Bytes()))
+		}
+
+		var discovery *subspacerelay.Discovery
+		discovery, err = subspacerelay.NewDiscovery(ctx, brokerURL,
+			subspacerelay.WithDiscoveryPayloadType(subspacerelaypb.PayloadType_PAYLOAD_TYPE_PCSC_READER),
+			subspacerelay.WithDiscoveryPrivateKey(key),
+		)
+		if err != nil {
+			slog.ErrorContext(ctx, "Error during discovery", rfid.ErrorAttrs(err))
+			os.Exit(1)
+		}
+
+		slog.InfoContext(ctx, "Waiting for discovery")
+		relayDiscovery := <-discovery.Chan()
+
+		// some implementations may opt to keep discovery running and continually service relays as they come online
+		// otherwise it is important to call Close to free resources
+		err = discovery.Close()
+		if err != nil {
+			slog.ErrorContext(ctx, "Error shutting down discovery", rfid.ErrorAttrs(err))
+			os.Exit(1)
+		}
+
+		slog.InfoContext(ctx, "Discovered relay", slog.String("relay_id", relayDiscovery.RelayId))
+		*relayID = relayDiscovery.RelayId
+	}
+
+	if *relayID == "" {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Subspace Relay Client ID (PCSC HF): ")
+		*relayID, err = reader.ReadString('\n')
+		if err != nil {
+			return
+		}
+		*relayID = strings.TrimSpace(*relayID)
+	}
+
+	// The connection type can be omitted to allow connecting to any PCSC reader (direct or regular), or changed to
+	// ConnectionType_CONNECTION_TYPE_PCSC_DIRECT to instead ensure the remote end is a direct connection
+	reader, err := subspacerelay.NewPCSC(ctx, brokerURL, *relayID, subspacerelaypb.ConnectionType_CONNECTION_TYPE_PCSC)
+	if err != nil {
+		slog.ErrorContext(ctx, "Error opening Subspace Relay PCSC connection", rfid.ErrorAttrs(err))
+		os.Exit(1)
+	}
+
+	err = run(ctx, reader, capduBytes)
+	if err != nil {
+		slog.ErrorContext(ctx, "Error", rfid.ErrorAttrs(err))
+		os.Exit(1)
+	}
+}
+
+func run(ctx context.Context, reader *subspacerelay.PCSC, capduBytes []byte) (err error) {
+	defer rfid.DeferWrap(ctx, &err)
+
+	// In your own code you might want to construct the cAPDU with github.com/nvx/go-apdu
+	//capduBytes, err := apdu.Capdu{
+	//	CLA: 0xFF,
+	//	INS: 0xCA, // GET DATA
+	//	P1:  0x00, // UID
+	//	P2:  0x00,
+	//	Ne:  apdu.MaxLenResponseDataStandard,
+	//}.Bytes()
+	//if err != nil {
+	//	return
+	//}
+
+	rapduBytes, err := reader.Exchange(ctx, capduBytes)
+	if err != nil {
+		return
+	}
+
+	rapdu, err := apdu.ParseRapdu(rapduBytes)
+	if err != nil {
+		return
+	}
+
+	slog.InfoContext(ctx, "Got response", "rapdu", rapdu)
+
+	return nil
+}
